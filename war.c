93dbc336bb25c40ef5e232d8a39ef587e0a4f7ca
WashingtonMelo <fcowashington@gmail.com>
2026-03-31 16:58:28 +0000

    Inclusão de bibliotecas

--- war.c
+++ war.c
@@ -3,3 +3,7 @@
 // Objetivo: Criar uma struct chamada Territorio que armazenará informações como nome, cor do exército e quantidade 
 // de tropas. O sistema permitirá o cadastro de 5 territórios e exibirá seus dados logo após o preenchimento. 
 //=====================================================================
+
+// Inclusão de bibliotecas necessárias
+#include <stdio.h>
+#include <string.h>
