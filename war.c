04f8b1f0d4820f956c0c3465766561883afab769
WashingtonMelo <fcowashington@gmail.com>
2026-03-31 17:05:49 +0000

    Criação da função principal com funções, entradas e saídas.

--- war.c
+++ war.c
@@ -17,3 +17,35 @@ struct Territorio {
 
 // Constante Global (Qtd total de territórios)
 #define TOTAL_TERRITORIOS 5
+
+// Função principal
+int main () {
+    struct Territorio territorios[TOTAL_TERRITORIOS];
+
+    //Laço para cadastrar os territórios
+    for (int i = 0; i < TOTAL_TERRITORIOS; i++) {
+        printf("\nCadastro do território %d:\n", i + 1);
+        printf("Digite o nome do território: ");
+        fgets(territorios[i].nome, 30, stdin);
+
+        printf("Digite a cor do exército: ");
+        fgets(territorios[i].corExercito, 10, stdin);
+
+        printf("Digite a quantidade de tropas: ");
+        scanf("%d", &territorios[i].qtdTropas);
+        getchar(); // Limpa o buffer de entrada
+    }
+
+    // Exibe os dados dos territórios cadastrados
+    printf("\n--- Territórios Cadastrados ---\n");
+    
+    for (int i = 0; i < TOTAL_TERRITORIOS; i++) {
+        printf("Território: %d\n", i + 1);
+        printf("Nome: %s", territorios[i].nome);
+        printf("Cor do Exército: %s", territorios[i].corExercito);
+        printf("Quantidade de Tropas: %d\n", territorios[i].qtdTropas);
+        printf("-----------------------------\n");
+    }
+
+    return 0; // Fim do programa
+}
